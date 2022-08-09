aa16cdfcbe11fd736f0c0aec6290d140bfbf02ff
Dmitry L <ds-sev@ya.ru>
2022-08-09 15:21:36 +0300

    add Task 13 about find third digit from entered number

---
+++ Task13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: 
+// Напишите программу, которая 
+// 1. выводит третью цифру заданного числа 
+// 2. или сообщает, что третьей цифры нет. 
+// Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a / 100 == 0  )           
+{
+    Console.WriteLine($"Треья цифра во введенном числе {a} отсутствует."); 
+    return;                                                                
+}
+int threeDigitNum = a;                                                     
+                                                                           
+while ( threeDigitNum / 100 > 10 || threeDigitNum / 100 < -10)             
+{
+    threeDigitNum = threeDigitNum / 10;
+}
+if (threeDigitNum > 0)
+{
+    Console.WriteLine($"Третья цифра введенного числа {a}: {threeDigitNum % 10}");
+}    
+else
+{
+    Console.WriteLine($"Третья цифра введенного числа {a}: {(threeDigitNum % 10) * -1}");
+}
+    
